a679315e94540d8e73175952c47e2cf3bd2da634
Ali <ahamidzadeh93@gmail.com>
2025-04-02 21:24:46 +0330

    [debug]: hash passwords in create user and change password

---
+++ Flask_Blog_Application/createUser.py
@@ -0,0 +1,20 @@
+#Create App Context & DB
+from app import create_app, db
+
+app = create_app()
+app.app_context().push()
+db.create_all()
+print("DB created!")
+
+#Create Admin User
+from app.models import User
+import hashlib
+
+raw_password = 'admin123'
+hashed_password = hashlib.md5(raw_password.encode()).hexdigest()
+
+admin = User(username='admin', password=hashed_password, role='admin')
+db.session.add(admin)
+db.session.commit()
+print("Admin user created!")
+print(f"Users: {User.query.all()}")
